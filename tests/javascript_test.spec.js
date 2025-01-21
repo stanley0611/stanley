e2942cebc6412cca31d49e46176baf8d9e32d5f6
stanley0611 <you@example.com>
2025-01-22 02:11:55 +0530

    Added reverse in javastring

--- tests/javascript_test.spec.js
+++ tests/javascript_test.spec.js
@@ -1,42 +1,75 @@
-const {test,expect} = require ('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
-test('test', async()=>{
+test('test', async () => {
 
-var x = 3
-var y = "3"
-console.log(x+y);
+  var x = 3
+  var y = "3"
+  console.log(x + y);
 
 
-
-})
-
-test('Boolean', async() => {
-var x =24
-var y = 0
-
-if(x){console.log(x)};
-if(y){console.log(y)};
 
 })
 
-test.only('coercion', async() => {
-var x = 220;
-var y = "Hello";
-var z = undefined;
-        
-x || y;    // Returns 220 since the first value is truthy
-        
-x || z;   // Returns 220 since the first value is truthy
-        
-x && y    // Returns "Hello" since both the values are truthy
-        
-y && z   // Returns undefined since the second value is falsy
-        
-if( x && y ){ 
-  console.log(x && y); // This block runs because x && y returns "Hello" (Truthy)
-}   
-        
-if( x || z ){
-  console.log("Code runs");  // This block runs because x || y returns 220(Truthy)
-}
+test('Boolean', async () => {
+  var x = 24
+  var y = 0
+
+  if (x) { console.log(x) };
+  if (y) { console.log(y) };
+
+})
+
+test('coercion', async () => {
+  var x = 220;
+  var y = "Hello";
+  var z = undefined;
+
+  x || y;    // Returns 220 since the first value is truthy
+
+  x || z;   // Returns 220 since the first value is truthy
+
+  x && y    // Returns "Hello" since both the values are truthy
+
+  y && z   // Returns undefined since the second value is falsy
+
+  if (x && y) {
+    console.log(x && y); // This block runs because x && y returns "Hello" (Truthy)
+  }
+
+  if (x || z) {
+    console.log("Code runs");  // This block runs because x || y returns 220(Truthy)
+  }
+})
+
+test("Javascript reverse", async () => {
+
+  function reverseString(str) {
+    return str.split('').reverse().join('');
+  }
+
+  let result = reverseString("Hello World");
+  console.log(result); // Output: "dlroW olleH"
+
+})
+
+
+test("Reverse String", async () => {
+  function reverse(str) {
+    return str.split('').reverse('').join('');
+  }
+
+  let result = reverse("stanley christopher");
+
+  console.log(result);
+
+})
+
+
+test.only("Reverse Array", async () => {
+  function reverseArray(arr) {
+    return arr.reverse('');
+  }
+  let numbers = [90, 80, 70, 60];
+  let reverse = reverseArray(numbers);
+  console.log(reverse);
 })
